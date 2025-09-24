d192aad6fca2e80c34219332dbd39f42c367edca
Programmer Nayeem <nayemnijami@gmail.com>
2025-09-24 15:22:37 +0600

    fix: resolve Google login account conflict issue

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -2,9 +2,7 @@ import requests as rq
 from google.auth.transport import requests
 from google.oauth2 import id_token
 from .models import User
-from django.contrib.auth import authenticate
 from django.conf import settings
-from rest_framework.exceptions import AuthenticationFailed
 from .exceptions import CustomAuthenticationFailed
 
 
@@ -33,38 +31,42 @@ class Facebook():
             print("Token error: ", e)
             return {}
 
+
 def register_social_user(provider, email, full_name):
-    old_user=User.objects.filter(email=email)
-    if old_user.exists():
-        if provider == old_user[0].auth_provider:
-            register_user=authenticate(email=email, password=settings.SOCIAL_AUTH_PASSWORD)
-            tokens = register_user.tokens()
-            
-            return {
-                'full_name':register_user.full_name,
-                'email':register_user.email,
-                'access_token':str(tokens.get('access')),
-                'refresh_token': str(tokens.get('refresh'))
-            }
-        else:
-            raise CustomAuthenticationFailed(f"Please continue your login with {old_user[0].auth_provider}")
-    else:
-        new_user={
-            'email':email,
-            'full_name':full_name,
-            'password':settings.SOCIAL_AUTH_PASSWORD
+    try:
+        user = User.objects.get(email=email)
+
+        if user.auth_provider != provider:
+            user.auth_provider = provider
+            user.is_verified = True
+            user.save()
+
+        tokens = user.tokens()
+
+        return {
+            'email': user.email,
+            'full_name': user.full_name,
+            "access_token": str(tokens.get('access')),
+            "refresh_token": str(tokens.get('refresh'))
         }
 
-        user=User.objects.create_user(**new_user)
-        user.auth_provider=provider
-        user.is_verified=True
+    except User.DoesNotExist:
+        new_user = {
+            'email': email,
+            'full_name': full_name,
+            'password': settings.SOCIAL_AUTH_PASSWORD
+        }
+
+        user = User.objects.create_user(**new_user)
+        user.auth_provider = provider
+        user.is_verified = True
         user.save()
-        login_user=authenticate(email=email, password=settings.SOCIAL_AUTH_PASSWORD)
-        tokens=login_user.tokens()
-        
+
+        tokens = user.tokens()
+
         return {
-            'email':login_user.email,
-            'full_name':login_user.full_name,
-            "access_token":str(tokens.get('access')),
-            "refresh_token":str(tokens.get('refresh'))
+            'email': user.email,
+            'full_name': user.full_name,
+            "access_token": str(tokens.get('access')),
+            "refresh_token": str(tokens.get('refresh'))
         }
